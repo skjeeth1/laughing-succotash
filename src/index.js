306f1659ced11b48512b042924a4b1c2b8f09dcd
skjeeth1 <skjeeth1@gmail.com>
2025-03-01 16:16:42 +0530

    Added navigation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import homepage from "./views/home";
 import aboutpage from "./views/about";
 import navbar from "./navbar";
 
-// homepage();
-// aboutpage();
 navbar();
+
+const doc = document.querySelector("#app");
+
+const homebutton = document.querySelector("#homebutton");
+homebutton.addEventListener("click", (e) => {
+    doc.innerHTML = "";
+    homepage();
+});
+
+const aboutbutton = document.querySelector("#aboutbutton");
+aboutbutton.addEventListener("click", (e) => {
+    doc.innerHTML = "";
+    aboutpage();
+});
--- src/navbar.js
+++ src/navbar.js
@@ -5,11 +5,13 @@ const navbar = () => {
     const but1 = document.createElement("button");
     but1.innerText = "Home";
     but1.classList.add("navbutton");
+    but1.id = "homebutton";
     div.appendChild(but1);
 
     const but2 = document.createElement("button");
     but2.innerText = "About";
     but2.classList.add("navbutton");
+    but2.id = "aboutbutton";
     div.appendChild(but2);
 
     doc.appendChild(div);
